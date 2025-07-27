745b9b3b49f9b564794dd849e7a6bbd73e74fe1a
Valdeirx <119995477+Valdeirx@users.noreply.github.com>
2025-07-27 12:02:54 -0400

    Update app.js
    update app.js - parte 2

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
-module.exports = app;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use the router for handling routes
 app.use('/', indexRouter);
 
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+// Export the app for Vercel to handle
+module.exports = app;
