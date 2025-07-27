9a1aa522908d8e0d391f31a49e0eed5c3b0711ce
Valdeirx <119995477+Valdeirx@users.noreply.github.com>
2025-07-27 17:56:06 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const router = express.Router();
 
 let mensagemAtual = "";
 
-app.post('/mensagem', (req, res) => {
+router.post('/mensagem', (req, res) => {
   mensagemAtual = req.body.mensagem;
   res.json({ status: 'Mensagem atualizada com sucesso!' });
 });
 
-app.get('/mensagem', (req, res) => {
+router.get('/mensagem', (req, res) => {
   res.json({ mensagem: mensagemAtual });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+module.exports = router;
